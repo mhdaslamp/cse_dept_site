daeaafdc73e8de8925a02114d38d02157dfbeb69
Jerald-Joyson <jeraldjoyson.inc@gmail.com>
2024-02-04 12:45:48 +0530

    hod message is set without responsive

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import CourseOfferedSection from "@/components/CourseOfferedSection";
 import Footer from "@/components/Footer";
+import HodMessage from "@/components/HodMessage";
 
 export default function Home() {
   return (
     <main>
       <CourseOfferedSection />
+      <HodMessage />
       <Footer />
     </main>
   );
---
+++ src/constants/contents.js
@@ -0,0 +1,39 @@
+export const hodName = "DR. SABITHA S";
+
+export const sampleText = `
+  Lorem ipsum dolor sit amet consectetur. Turpis consequat nec et suscipit in urna rhoncus. Ut amet ut arcu in ornare urna blandit. Sollicitudin sit est a odio. Eros vulputate etiam euismod risus augue feugiat in quis. Est molestie tristique feugiat faucibus tortor.Tortor nibh nunc congue consequat. Eleifend mauris magna egestas ultrices porta adipiscing turpis nunc praesent. Tortor phasellus diam arcu a massa. Cras lacus nec tellus nulla consectetur rhoncus tristique pellentesque montes. Dictumst sit vel faucibus egestas. Vehicula consectetur nisl in nulla venenatis.
+`;
+export const studentsData = [
+  {
+    name: "Muhammed Ali",
+    batch: "2021-25 Batch",
+    cgpa: "CGPA 9.3",
+    image: "./student1.jpg",
+  },
+  {
+    name: "VISWAJITH",
+    batch: "2021-25 Batch",
+    cgpa: "CGPA 9.3",
+    image: "./student1.jpg",
+  },
+  {
+    name: "VISWAJITH",
+    batch: "2021-25 Batch",
+    cgpa: "CGPA 9.3",
+    image: "./student1.jpg",
+  },
+  {
+    name: "VISWAJITH",
+    batch: "2021-25 Batch",
+    cgpa: "CGPA 9.3",
+    image: "./student1.jpg",
+  },
+  {
+    name: "jerald",
+    batch: "2021-25 Batch",
+    cgpa: "CGPA 9.3",
+    image: "./student1.jpg",
+  },
+];
+
+export default studentsData;
