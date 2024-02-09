b355aa7bc829e9ccfd5141ea0f7603baac611985
Viswajith2003 <viswajithviswa715@gmail.com>
2024-02-09 23:04:02 +0530

    Notifcation icon added

--- src/components/HomePage/page.js
+++ src/components/HomePage/page.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from "react";
+import { IoIosNotifications } from "react-icons/io";
 // import Styles from "globals.css";
 import Navbar from "../HomePage/Navbar/page";
 
@@ -14,11 +15,24 @@ function Home() {
           GOVERNMENT ENGINEERING COLLEGE, SREEKRISHNAPURAM, PALAKKAD
         </p>
       </div>
+      <a>
+        <IoIosNotifications
+          className="bottom-48 absolute mt-48 right-1 box-content px-3 py-2 border-2 border-black bg-white rounded-lg"
+          size={30}
+        />
+      </a>
+
       <div className="overflow-hidden relative w-full h-screen">
-        <video src="bg.mp4" autoPlay muted loop className="w-full h-full object-cover absolute top-0 z-[-1]" />
+        <video
+          src="bg.mp4"
+          autoPlay
+          muted
+          loop
+          className="w-full h-full object-cover absolute top-0 z-[-1]"
+        />
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
