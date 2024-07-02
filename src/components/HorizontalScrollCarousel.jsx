e5777a26295ef47df0d61a1a4696136a1fd3249b
Jerald-Joyson <jeraldjoyson.inc@gmail.com>
2024-07-02 09:50:49 +0530

    horixzontal scrool for tab screen is set

--- src/components/HorizontalScrollCarousel.jsx
+++ src/components/HorizontalScrollCarousel.jsx
@@ -8,12 +8,15 @@ const HorizontalScrollCarousel = () => {
     target: targetRef,
   });
 
-  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-35%"]);
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "-67%"]);
 
   return (
     <section ref={targetRef} className="relative h-[300vh] bg-neutral-900">
       <div className="sticky top-0 flex h-screen items-center overflow-hidden">
-        <motion.div style={{ x }} className="flex gap-10">
+        <motion.div
+          style={{ x }}
+          className="flex gap-1 h-screen w-auto border-2 border-red-500 p-8"
+        >
           <Card
             card={{
               url: "/ImageScroll/image1.png",
@@ -22,6 +25,7 @@ const HorizontalScrollCarousel = () => {
               title: "Title 1",
               id: 1,
             }}
+            position="top-0 right-0"
           />
           <Card
             card={{
@@ -31,6 +35,7 @@ const HorizontalScrollCarousel = () => {
               title: "Title 2",
               id: 2,
             }}
+            position="bottom-0 right-0"
           />
           <Card
             card={{
@@ -40,6 +45,7 @@ const HorizontalScrollCarousel = () => {
               title: "Title 3",
               id: 3,
             }}
+            position="top-0 right-0"
           />
           <Card
             card={{
@@ -49,6 +55,7 @@ const HorizontalScrollCarousel = () => {
               title: "Title 4",
               id: 4,
             }}
+            position="bottom-0 right-0"
           />
         </motion.div>
       </div>
@@ -58,21 +65,23 @@ const HorizontalScrollCarousel = () => {
 
 export default HorizontalScrollCarousel;
 
-const Card = ({ card }) => {
+const Card = ({ card, position }) => {
   return (
-    <div
-      key={card.id}
-      className="group relative overflow-hidden bg-neutral-200"
-      style={{ height: card.height, width: card.width }}
-    >
+    <div className="main-card w-[800px] h-auto relative border-2 border-emerald-500">
       <div
-        style={{
-          backgroundImage: `url(${card.url})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-        className="absolute inset-0 z-0 transition-transform duration-300 ease-in-out group-hover:scale-100"
-      ></div>
+        key={card.id}
+        className={`group absolute overflow-hidden bg-neutral-200 ${position} border-2 border-orange-500`}
+        style={{ height: card.height, width: card.width }}
+      >
+        <div
+          style={{
+            backgroundImage: `url(${card.url})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+          className="absolute inset-0 z-0 transition-transform duration-300 ease-in-out group-hover:scale-100"
+        ></div>
+      </div>
     </div>
   );
 };
