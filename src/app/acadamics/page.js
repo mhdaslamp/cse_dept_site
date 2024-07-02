04b21baeffdc19927af51dea20e8e8722ec25c01
mohamednishanm <mohamednishanm@gmail.com>
2024-07-02 11:26:31 +0530

    first commit

---
+++ src/app/acadamics/page.js
@@ -0,0 +1,327 @@
+import Navbar from '@/components/HomePage/Navbar/page'
+import React from 'react'
+
+const Acadamics = () => {
+  return (
+    <div className="bg-white w-full px-6 py-8 md:px-20 xl:px-24 sm:px-10">
+      Navbar
+      <div className="">
+
+        <div className="">
+
+          <div className="">
+            <h1>. Program education objectives - peos</h1>
+            <h2>Within a short span of time after graduation, the graduates shall</h2>
+            <ul>
+              <li>PEO1. Apply good analytic, design, and implementation skills required to formulate and solve computer science problems.</li>
+              <li>PEO2. Be employed as computer science professionals beyond entry-level positions.</li>
+              <li>PEO3. Be able to route their talents in to post graduate and research programs, promoting remarkable advancements in emerging areas.</li>
+              <li>PEO4. Demonstrate that they can function, communicate, collaborate and continue to learn effectively as ethically and socially responsible computer science professionals.</li>
+            </ul>
+          </div>
+
+          <div className="">
+            <h1>. Program specific outcomes - pSos</h1>
+            <ul>
+              <li>PSO1: Problem Solving Skills: Ability to design and develop computer programs and computer based systems of moderate complexity in the areas pertaining to system software, multimedia, database, networking, artificial intelligence, web- design and information security.</li>
+              <li>PSO2: Professional Skills: Ability to apply standard practices and methods in software project management and software development using suitable programming environments to deliver quality product for the industry..</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="">
+          <h1>. Program outcomes - pos</h1>
+          <h2>Engineering Graduates will be able to: </h2>
+          <ul>
+            <li>PO 1. Engineering knowledge: Apply the knowledge of mathematics, science, engineering fundamentals, and an engineering specialization to the solution of complex engineering problems.</li>
+            <li>PO 2. Problem analysis: Identify, formulate, review research literature, and analyze complex engineering problems reaching substantiated conclusions using first principles of mathematics, natural sciences, and engineering sciences.</li>
+            <li>PO 3. Design/development of solutions: Design solutions for complex engineering problems and design system components or processes that meet the specified needs with appropriate consideration for the public health and safety, and the cultural, societal, and environmental considerations.</li>
+            <li>PO 4. Conduct investigations of complex problems: Use research-based knowledge and research methods including design of experiments, analysis and interpretation of data, and synthesis of the information to provide valid conclusions.</li>
+            <li>PO 5. Modern tool usage: Create, select, and apply appropriate techniques, resources, and modern engineering and IT tools including prediction and modeling to complex engineering activities with an understanding of the limitations.</li>
+            <li>PO 6. The engineer and society: Apply reasoning informed by the contextual knowledge to assess societal, health, safety, legal and cultural issues and the consequent responsibilities relevant to the professional engineering practice.</li>
+            <li>PO 7. Environment and sustainability: Understand the impact of the professional engineering solutions in societal and environmental contexts, and demonstrate the knowledge of, and need for sustainable development.</li>
+            <li>PO 8. Ethics: Apply ethical principles and commit to professional ethics and responsibilities and norms of the engineering practice.</li>
+            <li>PO 9. Individual and team work: Function effectively as an individual, and as a member or leader in diverse teams, and in multidisciplinary settings.</li>
+            <li>PO 10. Communication: Communicate effectively on complex engineering activities with the engineering community and with society at large, such as, being able to comprehend and write effective reports and design documentation, make effective presentations, and give and receive clear instructions.</li>
+            <li>PO 11. Project management and finance: Demonstrate knowledge and understanding of the engineering and management principles and apply these to one’s own work, as a member and leader in a team, to manage projects and in multidisciplinary environments.</li>
+            <li>PO 12. Life-long learning: Recognize the need for, and have the preparation and ability to engage in independent and life-long learning in the broadest context of technological change.</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div className="">
+        <div className="">
+          <h1>PROGRAMMES & SYLLABI</h1>
+          <p>The Department of Computer Science & Engineering (CSE) offers a comprehensive range of programs designed to equip students with the knowledge and skills necessary to thrive in the ever-evolving field of computer science.</p>
+        </div>
+        <div className="">
+
+          <div className="">
+            <img
+              className=""
+              src="https://via.placeholder.com/400"
+              alt="Card Image"
+            />
+            <div className="p-6">
+              <h2 className="">SYLLABUS (2015 SCHEME)</h2>
+              <h2 className="">SYLLABUS (2019 SCHEME)</h2>
+              <h1 className="">BTech Computer Science And Engineering</h1>
+              <p className="">
+                The program started in 1999 with an approved intake of 66, consisting of 6 Regulars and 6 LET officers. Affiliation Kerala Technological University [KTU]
+              </p>
+            </div>
+          </div>
+
+          <div className="">
+            <img
+              className=""
+              src="https://via.placeholder.com/400"
+              alt="Card Image"
+            />
+            <div className="p-6">
+              <h2 className="">SYLLABUS (2015 SCHEME)</h2>
+              <h2 className="">SYLLABUS (2019 SCHEME)</h2>
+              <h1 className="">M.Tech Computational Linguistics</h1>
+              <p className="">
+                The program commenced in 2011 with an approved intake of 18 students. Affiliation Kerala Technological University [KTU]
+              </p>
+            </div>
+          </div>
+
+          <div className="">
+            <img
+              className=""
+              src="https://via.placeholder.com/400"
+              alt="Card Image"
+            />
+            <div className="p-6">
+              <h2 className="">SYLLABUS (2015 SCHEME)</h2>
+              <h2 className="">SYLLABUS (2019 SCHEME)</h2>
+              <h1 className="">PhD Computer Science And Engineering</h1>
+              <p className="">
+                The program began in 2016. Information about the approved intake is currently unavailable. Affiliation Kerala Technological University [KTU]
+              </p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <div className="">
+
+        <div className="">
+          <h1>Couses In Btech</h1>
+
+          <div className="">
+            <h1>S1 & S2</h1>
+            <div class="">
+              <div class="">
+                <div class="">
+                  <span class="">[1]</span>
+                  <div>
+                    <div class="">
+                      <span class="">MAT101 Linear Algebra and Calculus</span>
+                      <a href="your-link-here" class="button">SYLLABUS</a>
+                    </div>
+                    <h2 class="">The program commenced in 2011 with an approved intake of 18 students. Affiliation Kerala Technological University [KTU]</h2>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="">
+            <h1>S3</h1>
+            <div class="">
+              <div class="">
+                <div class="">
+                  <span class="">[1]</span>
+                  <div>
+                    <div class="">
+                      <span class="">MAT101 Linear Algebra and Calculus</span>
+                      <a href="your-link-here" class="button">SYLLABUS</a>
+                    </div>
+                    <h2 class="">The program commenced in 2011 with an approved intake of 18 students. Affiliation Kerala Technological University [KTU]</h2>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="">
+          <img src="https://via.placeholder.com/400" alt="Descriptive Alt Text" className="your-image-class"/>
+          <h1 className="your-title-class">. btech computer science</h1>
+        </div>
+
+
+      </div>
+
+      <div className="">
+
+        <div className="">
+          <img src="https://via.placeholder.com/400" alt="Descriptive Alt Text" className="your-image-class"/>
+          <h1 className="your-title-class">. MTECH computer science</h1>
+        </div>
+
+        <div className="">
+          <h1>Couses In Mtech</h1>
+
+          <div className="">
+            <h1>S1 & S2</h1>
+            <div class="">
+              <div class="">
+                <div class="">
+                  <span class="">[1]</span>
+                  <div>
+                    <div class="">
+                      <span class="">MAT101 Linear Algebra and Calculus</span>
+                      <a href="your-link-here" class="button">SYLLABUS</a>
+                    </div>
+                    <h2 class="">The program commenced in 2011 with an approved intake of 18 students. Affiliation Kerala Technological University [KTU]</h2>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+
+      </div>
+
+      <div className="">
+        <div className="">
+          <h1>LABS and Other Facilities</h1>
+          <p>The Department of Computer Science & Engineering (CSE) offers a comprehensive range of programs designed to equip students with the knowledge and skills necessary to thrive in the ever-evolving field of computer science.</p>
+        </div>
+        <div className="">
+
+          <div className="">
+            <img
+              className=""
+              src="https://via.placeholder.com/400"
+              alt="Card Image"
+            />
+            <div className="p-6">
+              <h1 className="">Smart Class Rooms</h1>
+              <p className="">
+                All the class rooms are equipped with LCD Projectors and Class room PA System
+              </p>
+            </div>
+          </div>
+
+
+          <div className="">
+            <img
+              className=""
+              src="https://via.placeholder.com/400"
+              alt="Card Image"
+            />
+            <div className="p-6">
+              <h1 className="">Department Seminar Hall</h1>
+              <p className="">
+                Computer Science and Engineering having a very good Seminar hall with HiFi sound systema and 3 X 3 video Wall. Online courses and MOOC programmes are conducted at seminar hall
+              </p>
+            </div>
+          </div>
+
+          <div className="">
+            <img
+              className=""
+              src="https://via.placeholder.com/400"
+              alt="Card Image"
+            />
+            <div className="p-6">
+              <h1 className="">Project Lab</h1>
+              <p className="">
+                Desktop - Intel Core i5/4GB/500GB(30Nos)
+                Desktop - AMD A10/4GB/500GB(14Nos)
+                Laptop - Intel Core i5/4GB/500GB(5Nos)
+              </p>
+            </div>
+          </div>
+
+          <div className="">
+            <img
+              className=""
+              src="https://via.placeholder.com/400"
+              alt="Card Image"
+            />
+            <div className="p-6">
+              <h1 className="">Software Lab</h1>
+              <p className="">
+                Server - Intel Xeon based (1 No)
+                Desktop - Intel Core i5/4GB/500GB (33 Nos)
+                Desktop - AMD A10/4GB/500GB (1 No)
+                Apple iMac - Intel Core i5/4GB/500GB/27" (1 No)
+                MFP - Multifunctional Device (1 No)
+              </p>
+            </div>
+          </div>
+
+          <div className="">
+            <img
+              className=""
+              src="https://via.placeholder.com/400"
+              alt="Card Image"
+            />
+            <div className="p-6">
+              <h1 className="">Hardware Lab</h1>
+              <p className="">
+                Desktop - Intel Core 2 Duo/4GB/500GB (9 Nos)
+                Desktop - AMD A10/4GB/500GB (9 Nos)
+                Desktop - AMD Rayzen/4GB/1TB (10 Nos)
+                Microprocessor Kit - Micro Controller kit (58 Nos)
+              </p>
+            </div>
+          </div>
+
+          <div className="">
+            <img
+              className=""
+              src="https://via.placeholder.com/400"
+              alt="Card Image"
+            />
+            <div className="p-6">
+              <h1 className="">Network Lab</h1>
+              <p className="">
+                Desktop - HP Intel Core i5/4GB/500GB (25 Nos)
+                Desktop - Dell Intel Core i5/4GB/500GB (10 Nos)
+                Projector - LCD Projector (1 No)
+              </p>
+            </div>
+          </div>
+
+          <div className="">
+            <img
+              className=""
+              src="https://via.placeholder.com/400"
+              alt="Card Image"
+            />
+            <div className="p-6">
+              <h1 className="">PG Lab</h1>
+              <p className="">
+                HighEnd Workstation - Intel Xeon/64GB/1TB (3 Nos)
+                LowEnd Workstation - Intel Xeon/16GB/1TB (4 Nos)
+                Apple iMac - Applce MAC 27" (3 Nos) 
+                Desktop - Intel Core i5/8GB/1TB (10 Nos)
+                Laptop - Intel Core i5/4GB/500GB (10 Nos)
+              </p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    {/* footer */}
+    </div>
+  )
+}
+
+export default Acadamics
+
+
+
+  
