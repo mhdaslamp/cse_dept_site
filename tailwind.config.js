a9c9721b87565599061252f261483317649738ad
Viswajith2003 <viswajithviswa715@gmail.com>
2024-07-12 21:41:37 +0530

    notifcation div set

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,19 +9,13 @@ module.exports = {
     extend: {
       fontFamily: {
         montserrat: "var(--font-montserrat), monospace",
-        bebasneue: "var(--font-bebasneue), monospace"
+        bebasneue: "var(--font-bebasneue), monospace",
       },
       fontSize: {
-        "5.5xl": "3.5rem"
+        "5.5xl": "3.5rem",
       },
-      colors:{
-        'newblue':'#0D38B1',
-      },
-      backgroundColor: {
-        'white-trans-20': 'rgba(255, 255, 255, 0.10)',
-      },
-      backdropFilter: {
-        'blur-32': 'blur(32.4px)',
+      colors: {
+        newblue: "#0D38B1",
       },
     },
   },
