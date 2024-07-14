6cb52286c50025bafd9f5856e32030a208fcbe08
Jerald-Joyson <jeraldjoyson.inc@gmail.com>
2024-07-14 21:51:51 +0530

    Toppers section is changed and it is responsive

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import DeptInfo from "@/components/DeptInfo";
 import DeptLogo from "@/components/DeptLogo";
 import Footer from "@/components/Footer";
 import HodMessage from "@/components/HodMessage";
-import Toppers from "@/components/Toppers/Toppers";
+import Toppers from "@/components/Toppers";
 import HomePage from "@/components/HomePage/page";
 import References from "@/components/References";
 import HorizontalScrollCarousel from "@/components/HorizontalScrollCarousel";
@@ -23,6 +23,3 @@ export default function Home() {
     </main>
   );
 }
-
-
-
