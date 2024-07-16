04a71fb5eb7255e53dd4ec054a9f7a4e72446b5f
Viswajith2003 <viswajithviswa715@gmail.com>
2024-07-16 19:10:55 +0530

    mobile nav 98% setted

--- src/components/HomePage/Navbar/NavbarItem/page.jsx
+++ src/components/HomePage/Navbar/NavbarItem/page.jsx
@@ -14,11 +14,11 @@ function NavbarItem({
 
   return (
     <li
-      className="relative md:pb-5 text-[18px] my-8 md:px-6 text-left border-b-2 md:border-b-0 md:hover:bg-transparent hover:scale-x-105"
+      className="relative md:pb-5 text-[18px] my-8 md:my-0 md:px-6 text-left border-b-2 md:border-b-0 md:hover:bg-transparent hover:scale-x-105"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <div className="flex items-center justify-between w-full text-black md:text-white ">
+      <div className="flex items-center justify-between w-full text-black md:text-white">
         {isHovered && (
           <div className="dot w-2 h-2 absolute top-3 left-1 lg:bg-white rounded"></div>
         )}
@@ -40,12 +40,14 @@ function NavbarItem({
             background: "rgba(255, 255, 255, 0.10)",
             backdropFilter: "blur(30px)",
           }}
-          className="absolute left-0 mt-2  py-2 shadow-lg w-52"
+          className={`md:absolute left-0 mt-2 md:ml-0 py-2 shadow-lg w-full md:w-52 ${
+            isHovered ? "block" : "hidden"
+          } md:block`}
         >
           {dropdownItems.map((item, index) => (
             <li
               key={index}
-              className="py-2  px-4 hover:bg-slate-500 hover:bg-opacity-10 hover:scale-95"
+              className="py-2 px-4 md:px-2 hover:bg-slate-500 hover:bg-opacity-10 hover:scale-95"
             >
               <Link href={item.href} onClick={() => setNavbar(false)}>
                 {item.label}
--- src/components/HomePage/Navbar/page.jsx
+++ src/components/HomePage/Navbar/page.jsx
@@ -39,7 +39,7 @@ function Navbar() {
       }
       className="md:flex lg:mt-8 max-w-screen-2xl mx-auto font-bold fixed top-0 left-0 right-0 z-10 h-20"
     >
-      <div className="flex items-center justify-between py-3 px-4 md:px-2 h-20 mx-auto ">
+      <div className="flex items-center justify-between py-3 px-4 md:px-2 h-20 mx-auto">
         <div className="flex gap-4 items-center md:mr-14">
           <Image
             src={open ? "/logo(black).png" : "/logo.png"}
