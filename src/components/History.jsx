83010b19f6fedd9c83ddcd53439976528b4c8300
Viswajith2003 <viswajithviswa715@gmail.com>
2024-07-23 11:12:46 +0530

    about-history page starting

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 export default function History() {
   return (
-    <div>
-      
+    <div className="mt-24 space-y-24">
+      <div className="flex flex-row mx-24 space-x-10">
+        <div className="w-1/2">
+          <h1 className="font-bold text-3xl">History of the department</h1>
+          <p className="mt-2 text-start text-2xl text-gray-500">
+            At the heart of the digital revolution lies the computer science
+            department. We are the architects of the future, unraveling the
+            mysteries of computation and transforming them into groundbreaking
+            technologies. Our department offers a thrilling exploration into the
+            world of computers and computational systems, encompassing
+            everything from the foundational algorithms that power the digital
+            age to the cutting-edge advancements in artificial intelligence and
+            cybersecurity.
+          </p>
+        </div>
+        <div className="w-1/2">
+          <Image
+            src="/history1.png" // Path to your image
+            alt="picture"
+            width={400} // Desired width
+            height={300} // Desired height
+            className="ml-80"
+          />
+        </div>
+      </div>
+      <div className="flex flex-row mx-24 space-x-10">
+        <div>
+          <Image
+            src="/history2.png" // Path to your image
+            alt="picture"
+            width={1300} // Desired width
+            height={800} // Desired height
+          />
+        </div>
+        <div>
+          <p className="mt-2 text-start text-xl">
+            Whether you're fascinated by the intricate dance of data structures
+            or dream of crafting the next generation of software, our curriculum
+            equips you with the essential tools. We delve into the theoretical
+            underpinnings of computer science, providing a rock-solid
+            understanding of algorithms, programming languages, and the
+            principles that govern how information is processed. This knowledge
+            becomes the springboard for practical application. You'll not only
+            learn to code but also to design, analyze, and build complex
+            software systems.
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/aboutUs/page.jsx
+++ src/components/aboutUs/page.jsx
@@ -8,14 +8,16 @@ import HomePage from "@/components/HomePage/page";
 import References from "@/components/References";
 import HorizontalScrollCarousel from "@/components/HorizontalScrollCarousel";
 import Acadamics from "@/app/acadamics/page";
+import History from "../History";
 
 export default function About() {
   return (
     <main>
-      <HomePage />
+      {/* <HomePage />
       <DeptInfo />
       <DeptLogo />
-      <Acadamics/>
+      <Acadamics/> */}
+      <History/>
       
       {/*
       <HorizontalScrollCarousel />
