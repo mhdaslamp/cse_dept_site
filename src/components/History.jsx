39cd09b51ab48e4c77ad780295c247f5bb31d463
Viswajith2003 <viswajithviswa715@gmail.com>
2024-07-23 11:24:48 +0530

    Histroy responsive completed

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 
 export default function History() {
   return (
-    <div className="mt-24 space-y-24">
-      <div className="flex flex-row mx-28 space-x-10">
-        <div className="w-1/2">
+    <div className="mt-24 md:space-y-24 px-4 md:px-28">
+      <div className="flex flex-col md:flex-row space-y-10 md:space-y-0 md:space-x-10">
+        <div className="w-full md:w-1/2">
           <h1 className="font-bold text-3xl">History of the department</h1>
-          <p className="mt-5 text-start text-2xl text-gray-500">
+          <p className="mt-5 text-start text-xl md:text-2xl text-gray-500">
             At the heart of the digital revolution lies the computer science
             department. We are the architects of the future, unraveling the
             mysteries of computation and transforming them into groundbreaking
@@ -18,28 +18,28 @@ export default function History() {
             cybersecurity.
           </p>
         </div>
-        <div className="w-1/2">
+        <div className="w-full md:w-1/2 hidden md:block">
           <Image
             src="/history1.png" // Path to your image
-            alt="picture"
+            alt="History of the department"
             width={400} // Desired width
             height={300} // Desired height
-            className="ml-80"
+            className="mx-auto md:ml-80"
           />
         </div>
       </div>
-      <div className="flex flex-row mx-24 space-x-10">
-        <div className="w-1/2">
+      <div className="flex flex-col md:flex-row space-y-10 md:space-y-0 md:space-x-10">
+        <div className="w-full md:w-1/2 hidden md:block">
           <Image
             src="/history2.png" // Path to your image
-            alt="picture"
+            alt="History of the department"
             width={400} // Desired width
             height={300} // Desired height
-            className="mb-10 ml-10"
+            className="mx-auto md:ml-10"
           />
         </div>
-        <div className="w-1/2">
-          <p className="mt-5 text-start text-2xl text-gray-500">
+        <div className="w-full md:w-1/2">
+          <p className="mt-5 text-start text-xl md:text-2xl text-gray-500">
             Whether you're fascinated by the intricate dance of data structures
             or dream of crafting the next generation of software, our curriculum
             equips you with the essential tools. We delve into the theoretical
