65b4e876222a0bac1f3395991db87af1adbbf28b
Viswajith2003 <viswajithviswa715@gmail.com>
2024-07-23 11:17:51 +0530

    about-history page lg completed

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 export default function History() {
   return (
     <div className="mt-24 space-y-24">
-      <div className="flex flex-row mx-24 space-x-10">
+      <div className="flex flex-row mx-28 space-x-10">
         <div className="w-1/2">
           <h1 className="font-bold text-3xl">History of the department</h1>
-          <p className="mt-2 text-start text-2xl text-gray-500">
+          <p className="mt-5 text-start text-2xl text-gray-500">
             At the heart of the digital revolution lies the computer science
             department. We are the architects of the future, unraveling the
             mysteries of computation and transforming them into groundbreaking
@@ -29,16 +29,17 @@ export default function History() {
         </div>
       </div>
       <div className="flex flex-row mx-24 space-x-10">
-        <div>
+        <div className="w-1/2">
           <Image
             src="/history2.png" // Path to your image
             alt="picture"
-            width={1300} // Desired width
-            height={800} // Desired height
+            width={400} // Desired width
+            height={300} // Desired height
+            className="mb-10 ml-10"
           />
         </div>
-        <div>
-          <p className="mt-2 text-start text-xl">
+        <div className="w-1/2">
+          <p className="mt-5 text-start text-2xl text-gray-500">
             Whether you're fascinated by the intricate dance of data structures
             or dream of crafting the next generation of software, our curriculum
             equips you with the essential tools. We delve into the theoretical
