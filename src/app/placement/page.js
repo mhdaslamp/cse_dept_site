8dcaac1fe1f4456beb9045fa8e815ad67d6096ca
mohamednishanm <mohamednishanm@gmail.com>
2024-07-30 15:05:16 +0530

    error solved

---
+++ src/app/placement/page.js
@@ -0,0 +1,14 @@
+import Footer from '@/components/Footer'
+import Navbar from '@/components/HomePage/Navbar/page'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+        <Navbar/>
+        <Footer/>
+    </>
+  )
+}
+
+export default page
