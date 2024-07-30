07cede57a7d5689437c3b0b786071d99a6146535
Viswajith2003 <viswajithviswa715@gmail.com>
2024-07-30 18:55:49 +0530

    activity page setted

--- src/app/activity/student_gp/page.jsx
+++ src/app/activity/student_gp/page.jsx
@@ -3,11 +3,11 @@ import React from "react";
 export default function page() {
   return (
     <div className="">
-      <div className="bg-[#e9e8e9] w-full h-[350px] pl-20">
-        <span className="pt-36 w-7 h-4 bg-black"></span>
-        <h1 className="uppercase text-[48px] font-bold pt-36 ">
-          Student group
-        </h1>
+      <div className="bg-[#e9e8e9] ">
+        <div className="container mx-auto w-full  h-[350px] flex justify-start items-end pb-8">
+          <span className=" w-3 h-3 bg-black mb-5 mr-3"></span>
+          <h1 className="uppercase text-[48px] font-bold">Student group</h1>
+        </div>
       </div>
       <div></div>
     </div>
