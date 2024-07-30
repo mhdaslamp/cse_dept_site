53039d10be908e490dd585f7c2660778568331c5
Viswajith2003 <viswajithviswa715@gmail.com>
2024-07-30 16:02:09 +0530

    activity page started

---
+++ src/app/activity/student_gp/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="">
+      <div className="bg-[#e9e8e9] w-full h-[350px] pl-20">
+        <span className="pt-36 w-7 h-4 bg-black"></span>
+        <h1 className="uppercase text-[48px] font-bold pt-36 ">
+          Student group
+        </h1>
+      </div>
+      <div></div>
+    </div>
+  );
+}
