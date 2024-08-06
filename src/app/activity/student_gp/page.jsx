b62fdafbfbec19396cbc8499612f3f104dfc27b0
Viswajith2003 <viswajithviswa715@gmail.com>
2024-08-06 08:55:09 +0530

    activity modifing

--- src/app/activity/student_gp/page.jsx
+++ src/app/activity/student_gp/page.jsx
@@ -9,7 +9,7 @@ export default function Student() {
       <div className="bg-[#e9e8e9]">
         <div className="container mx-auto w-full h-[350px] flex justify-start items-end pb-8">
           <span className="w-3 h-3 bg-black mb-5 mr-3"></span>
-          <h1 className="uppercase text-[48px] font-bold">Student group</h1>
+          <h1 className="uppercase text-[48px] font-bold">Student groups</h1>
         </div>
       </div>
       {data.map((item, index) => (
