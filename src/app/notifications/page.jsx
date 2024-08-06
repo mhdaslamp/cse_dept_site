f41b041cec4eb20dd6ad5140c0232cd372aa6ed3
Viswajith2003 <viswajithviswa715@gmail.com>
2024-08-07 00:15:04 +0530

    navbar item updated

---
+++ src/app/notifications/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Notification() {
+  return (
+    <div>
+      <h1>This is the notification page</h1>
+    </div>
+  )
+}
---
+++ src/app/placements/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Placement() {
+  return (
+    <div>
+      <h1>This is the placement page</h1>
+    </div>
+  )
+}
