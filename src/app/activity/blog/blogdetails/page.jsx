847e5617a3c4fb84fd87b2ac7576ef2b94f37aca
Viswajith2003 <viswajithviswa715@gmail.com>
2024-08-10 17:27:58 +0530

    blog page details set

--- src/app/activity/blog/blogdetails/page.jsx
+++ src/app/activity/blog/blogdetails/page.jsx
@@ -27,19 +27,19 @@ export default function details(item) {
           />
         </div>
         <div className="grid grid-flow-col-dense  mt-8 ">
-          <div className="w-96 flex flex-col items-center text-center">
+          <div className="w-72 flex flex-col items-center text-center">
             <IoPersonCircle className="w-8 h-8" />
-            <h1>Jane Doe</h1>
-            <p>2025 Pass out Batch</p>
+            <h1 className="text-xl">Jane Doe</h1>
+            <p className="text-gray-600">2025 Pass out Batch</p>
           </div>
-          <div className="w-4/4 space-y-3 text-justify mr-2">
+          <div className="w-4/4 space-y-3 text-justify mr-2 text-gray-600">
             In an era marked by rapid technological advancements, the education
             sector is undergoing a significant transformation. Digital
             technologies are revolutionizing the way we teach and learn, making
             education more accessible, engaging, and personalized. This blog
             explores the impact of digital transformation on education and how
             it is reshaping the learning landscape.
-             <br />
+            <br />
             One of the most notable changes brought about by digital
             transformation is the shift from traditional classroom settings to
             more flexible, online learning environments. Interactive learning
@@ -49,26 +49,26 @@ export default function details(item) {
             offer a wide range of courses, from basic skills to advanced
             degrees, allowing learners to tailor their education to their
             individual needs and schedules.
-            <br /> 
-            Digital transformation has also introduced innovative
-            tools and technologies that enhance the learning experience. For
-            example, virtual reality (VR) and augmented reality (AR) are being
-            used to create immersive learning environments that can bring
-            complex subjects to life. Students can explore historical sites,
-            conduct virtual science experiments, and interact with 3D models,
-            making learning more engaging and interactive. Additionally,
-            AI-driven personalized education systems can analyze student
-            performance data to provide customized learning experiences,
-            identifying areas where students need improvement and offering
-            targeted resources to help them succeed.
             <br />
-            Collaboration and communication have also been
-            significantly improved through digital transformation. Online
-            collaboration tools, such as video conferencing, instant messaging,
-            and shared document platforms, enable students and teachers to work
-            together more effectively, regardless of geographical location.
-            These tools facilitate real-time feedback and support, creating a
-            more connected and.....
+            Digital transformation has also introduced innovative tools and
+            technologies that enhance the learning experience. For example,
+            virtual reality (VR) and augmented reality (AR) are being used to
+            create immersive learning environments that can bring complex
+            subjects to life. Students can explore historical sites, conduct
+            virtual science experiments, and interact with 3D models, making
+            learning more engaging and interactive. Additionally, AI-driven
+            personalized education systems can analyze student performance data
+            to provide customized learning experiences, identifying areas where
+            students need improvement and offering targeted resources to help
+            them succeed.
+            <br />
+            Collaboration and communication have also been significantly
+            improved through digital transformation. Online collaboration tools,
+            such as video conferencing, instant messaging, and shared document
+            platforms, enable students and teachers to work together more
+            effectively, regardless of geographical location. These tools
+            facilitate real-time feedback and support, creating a more connected
+            and.....
           </div>
         </div>
       </div>
