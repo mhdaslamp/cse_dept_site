9ffd0d5fa18578e8864e4bbec55791f50043e534
Viswajith2003 <viswajithviswa715@gmail.com>
2024-08-15 16:36:22 +0530

    activity event pages responsived

--- src/app/activity/events/[product]/page.jsx
+++ src/app/activity/events/[product]/page.jsx
@@ -21,15 +21,15 @@ export default function EventDetails() {
 
   return (
     <ColoredSection color="BLACK">
-      <div className="container min-h-screen bg-white mx-auto pb-14">
-        <div className="w-full flex flex-col justify-center items-center pt-[200px]">
+      <div className="container min-h-screen bg-white mx-auto pb-14 px-4 lg:px-0">
+        <div className="w-full flex flex-col justify-center items-center pt-[100px] md:pt-[150px] lg:pt-[200px]">
           <div className="w-full h-auto flex flex-row justify-center items-end">
             <span className="w-[10px] h-[10px] bg-black mr-3 mb-4"></span>
-            <h1 className="uppercase text-[40px] w-auto h-auto font-bold">
+            <h1 className="uppercase text-[28px] md:text-[35px] lg:text-[40px] w-auto h-auto font-bold">
               {event.title}
             </h1>
           </div>
-          <p className="text-gray-500 text-sm mb-4 uppercase">
+          <p className="text-gray-500 text-xs md:text-sm lg:text-base mb-4 uppercase">
             {new Date(event.date).toLocaleDateString("en-US", {
               year: "numeric",
               month: "long",
@@ -37,19 +37,21 @@ export default function EventDetails() {
             })}
           </p>
         </div>
-        <div className="w-full h-full flex flex-row mt-8">
-          <div className="w-[40%] h-full flex justify-center items-center">
+        <div className="w-full h-full flex flex-col lg:flex-row mt-8">
+          <div className="w-full lg:w-[40%] h-full flex justify-center items-center mb-6 lg:mb-0">
             <img
               src={event.image || "https://via.placeholder.com/600x400"}
               alt={event.title}
-              className="w-[70%] h-auto object-cover"
+              className="w-[80%] md:w-[70%] h-auto object-cover"
             />
           </div>
-          <div className="w-[50%] h-[300px] overflow-y-auto text-justify pr-4 scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100">
-            <p className="text-black text-lg">{event.descriptions}</p>
+          <div className="w-full lg:w-[50%] h-[300px] overflow-y-auto text-justify pr-4 scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100">
+            <p className="text-black text-base md:text-lg">
+              {event.descriptions}
+            </p>
           </div>
         </div>
-        <button className="w-[85%] h-auto p-3 mt-8 ml-16 bg-[#696969] text-white hover:border-red-500 hover:border-2 hover:bg-white hover:text-red-500 transition-all duration-300 flex justify-center items-center">
+        <button className="w-full md:w-[90%] lg:w-[88%]  h-auto p-3 mt-8 mx-auto bg-[#696969] text-white hover:border-red-500 hover:border-2 hover:bg-white hover:text-red-500 transition-all duration-300 flex justify-center items-center">
           <p className="uppercase text-sm font-semibold">register now</p>
         </button>
       </div>
