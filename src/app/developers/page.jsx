fbda72c7af0105d0cf841257a9e7e8dd725aa810
Viswajith2003 <viswajithviswa715@gmail.com>
2024-08-20 16:59:34 +0530

    develpers page seted

--- src/app/developers/page.jsx
+++ src/app/developers/page.jsx
@@ -38,27 +38,29 @@ export default function Developers() {
 
   return (
     <ColoredSection>
-      <div id="teams" className=" bg-[#0a0a0a] h-screen text-white px-40 pt-20">
+      <div id="teams" className="bg-[#0a0a0a] h-screen text-white px-40 pt-20">
         <h1 className="pt-8 text-[100px] font-semibold">Meet our team</h1>
-        <div className="flex gap-5">
-          {data.map((item, index) => (
-            <div key={index}>
-              <Image
-                src={item.img}
-                alt="developers"
-                width={300}
-                height={500}
-                className="rounded-lg"
-              />
-              <div className="mt-4">
-                <h3 className="text-4xl font-semibold">{item.name}</h3>
-                <div className="flex gap-2 mt-1">
-                  <div className="relative mt-2 w-4 h-4 bg-[#d9d9d9] rounded-lg " />
-                  <h2 className="text-2xl font-semibold ">{item.position}</h2>
+        <div className="overflow-x-auto whitespace-nowrap mt-6">
+          <div className="flex gap-4">
+            {data.map((item, index) => (
+              <div key={index} className="inline-block min-w-[300px]">
+                <Image
+                  src={item.img}
+                  alt="developers"
+                  width={500}
+                  height={200}
+                  className="rounded-lg w-auto h-[400px] object-cover"
+                />
+                <div className="mt-4">
+                  <h3 className="text-2xl font-semibold">{item.name}</h3>
+                  <div className="flex gap-2 mt-1">
+                    <div className="relative mt-2 w-4 h-4 bg-[#d9d9d9] rounded-lg " />
+                    <h2 className="text-xl font-semibold ">{item.position}</h2>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </ColoredSection>
