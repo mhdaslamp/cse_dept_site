d0184ed90f5b1468ab2b51d133b56f3507c78e60
mohamednishanm <mohamednishanm@gmail.com>
2024-08-20 12:20:28 +0530

    responsive 4

--- src/components/PlacementIntro.jsx
+++ src/components/PlacementIntro.jsx
@@ -1,33 +1,30 @@
-import Image from "next/image";
+"use client";
+
 import React from "react";
-import placementLogo from "../../public/placement-cell-logo.svg";
 import ColoredSection from "./ColoredSection";
 
-const PlacementIntro = () => {
+const HodMessage = () => {
   return (
-    <ColoredSection  color="BLACK">
-      <div className="bg-white container mx-auto py-56 px-4 h-full md:h-[70vh]">
-        <div className="w-full h-auto text-right">
-          <h1 className="text-black w-auto h-auto text-4xl lg:text-5xl font-semibold font-bebasneue">
-          . TRAINING & PLACEMENT
+    <ColoredSection color="BLACK">
+      <div className="bg-white container mx-auto py-32 md:py-32 px-4 h-full md:min-h-[70vh]">
+        <div className="w-full h-auto mb-8 md:mb-12">
+          <h1 className="text-black w-auto h-auto text-3xl md:text-4xl lg:text-5xl font-semibold font-bebasneue">
+            . TRAINING & PLACEMENT
           </h1>
         </div>
-        <div className="flex flex-col md:flex-row w-full h-auto md:p-5 xl:p-10">
-          {/* Image of hod and its desigination */}
-          <div className="flex-auto w-full md:w-2/6 h-2/4 pt-5 md:pt-0 items-center justify-center ">
-            <div className="w-full h-auto flex justify-center items-center">
+        <div className="grid md:grid-cols-[330px_auto] grid-cols-1 gap-8 md:gap-[70px] w-full h-auto">
+          <div className="w-full flex justify-center md:justify-start">
+            <div className="w-full max-w-[250px] md:max-w-none">
               <img
-                src={"./placement-cell-logo.svg"}
-                alt="HOD Image"
-                layout="responsive"
-                className="w-[200px] h-[250px] xl:w-[260px] xl:h-[320px] object-cover transition duration-300 ease-in-out hover:scale-105 "
+                src="./placement-cell-logo.svg"
+                alt="placement cell logo"
+                className="w-full h-auto object-contain transition duration-300 ease-in-out"
               />
             </div>
-
           </div>
           {/* message of hod */}
-          <div className="flex-auto justify-start w-full md:w-4/6 h-2/4 pt-5 md:pt-0 md:pl-5">
-            <p className="text-gray-500 text-[16px] sm:text-[17px] lg:text-[18px] xl:text-[22px] leading-[28px] md:leading-[30px] lg:leading-[40px] transition duration-300 ease-in-out hover:scale-105">
+          <div className="flex-auto w-full">
+            <p className="text-gray-500 text-[16px] sm:text-lg lg:text-xl xl:text-2xl leading-relaxed transition duration-300 ease-in-out">
               Lorem ipsum dolor sit amet consectetur. Turpis consequat nec et suscipit in urna rhoncus. Ut amet ut arcu in ornare urna blandit. Sollicitudin sit est a odio. Eros vulputate etiam euismod risus augue feugiat in quis. Est molestie tristique feugiat faucibus tortor.Tortor nibh nunc congue consequat. Eleifend mauris magna egestas ultrices porta adipiscing turpis nunc praesent. Tortor phasellus diam arcu a massa. Cras lacus nec tellus nulla consectetur rhoncus tristique pellentesque montes. Dictumst sit vel faucibus egestas. Vehicula consectetur nisl in nulla venenatis.
             </p>
           </div>
@@ -37,4 +34,4 @@ const PlacementIntro = () => {
   );
 };
 
-export default PlacementIntro;
+export default HodMessage;
