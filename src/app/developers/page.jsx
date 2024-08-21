4c7989ed9c4b47096625533e4b8a6ecb57fc5147
Viswajith2003 <viswajithviswa715@gmail.com>
2024-08-21 23:33:18 +0530

    develpers page seted

--- src/app/developers/page.jsx
+++ src/app/developers/page.jsx
@@ -48,7 +48,7 @@ export default function Developers() {
                   src={item.img}
                   alt="developers"
                   width={500}
-                  height={200}
+                  height={100}
                   className="rounded-lg w-auto h-[400px] object-cover"
                 />
                 <div className="mt-4">
