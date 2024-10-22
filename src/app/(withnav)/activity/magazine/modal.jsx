cb7ca02f0fa43579fbbb6746c7f363d604718fcf
Viswajith2003 <viswajithviswa715@gmail.com>
2024-10-23 01:29:06 +0530

    responsive the pending pages

--- src/app/(withnav)/activity/magazine/modal.jsx
+++ src/app/(withnav)/activity/magazine/modal.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 const Modal = ({ children, onClose }) => {
   return (
-    <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white p-6 rounded shadow-lg">
+    <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50 p-4">
+      <div className="bg-white p-4 sm:p-6 rounded shadow-lg w-full max-w-3xl relative">
         <button
           onClick={onClose}
-          className="absolute top-[8%]  right-[28%] p-3 bg-white text-black rounded-sm"
+          className="absolute top-2 right-2 sm:top-4 sm:right-4 p-2 sm:p-3 bg-white text-black rounded-sm hover:bg-gray-200 transition-colors"
         >
           Close
         </button>
-        {children}
+        <div className="mt-8 sm:mt-0">
+          {children}
+        </div>
       </div>
     </div>
   );
